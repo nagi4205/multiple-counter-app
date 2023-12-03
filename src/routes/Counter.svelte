de1da5ddd99749c31af61dc2fcdb57b5a365703b
nagi4205 <nagisome1234@gmail.com>
2023-12-03 21:38:41 +0900

    レスポンシブ対応するためにレイアウトを調整

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -50,19 +50,19 @@ const changeTitle = (counterId: number, event: Event): void => {
 
 <div class="flex flex-col items-center">
 	{#each counters as counter (counter.id)}
-	<div class="w-fit h-12 rounded border-solide bg-gray-100 shadow-xl flex items-center justify-between mb-2">
-		<input type="text" class="text-gray-500 mx-4 px-1 w-32" value={counter.title} on:input={(event) => changeTitle(counter.id, event)}>
-		<div class=" font-bold px-4">{counter.count}</div>
-			<div class="px-4">
-					<button on:click={() => increment(counter.id)} class="bg-red-500 rounded-l w-8 h-8 text-white" tabindex="-1">+</button>
-					<button on:click={() => decrement(counter.id)} class="bg-blue-500 w-8 h-8 text-white" tabindex="-1">-</button>
-					<button on:click={() => reset(counter.id)} class="bg-yellow-500 rounded-r w-8 h-8 text-white" tabindex="-1">0</button>
-					<button on:click={() => removeCounter(counter.id)} class="w-8 h-8 pl-4" tabindex="-1">×</button>
+	<div class="w-80 xs:w-96 h-12 rounded border-solide bg-gray-100 shadow-xl flex items-center justify-between mb-4">
+		<input type="text" class="text-gray-500 mx-2 xs:mx-4 w-32" value={counter.title} on:input={(event) => changeTitle(counter.id, event)}>
+		<div class=" font-bold px-2">{counter.count}</div>
+			<div class="px-2">
+					<button on:click={() => increment(counter.id)} class="bg-red-500 rounded-l w-6 h-6 text-white" tabindex="-1">+</button>
+					<button on:click={() => decrement(counter.id)} class="bg-blue-500 w-6 h-6 text-white" tabindex="-1">-</button>
+					<button on:click={() => reset(counter.id)} class="bg-yellow-500 rounded-r w-6 h-6 text-white" tabindex="-1">0</button>
+					<button on:click={() => removeCounter(counter.id)} class="w-6 h-6 pl-2" tabindex="-1">×</button>
 			</div>
 	</div>
 	{/each}
 	
-	<button class="max-w-sm w-80 rounded-md bg-green-400 text-white" on:click={addCounter} tabindex="-1">new counter</button>
+	<button class="w-80 xs:w-96 rounded-md bg-green-400 text-white" on:click={addCounter} tabindex="-1">new counter</button>
 	
 	<div class="w-full text-center">
 		<!-- <div>title list: {counters.map(c => c.title).join(', ')}</div> -->
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      xxs: '353px',
+      xs: '480px',
+    },
     extend: {},
   },
   plugins: [],
