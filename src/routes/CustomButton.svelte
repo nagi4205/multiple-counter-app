cb25892dff6045419aa204aac409d4a64bb2cf44
nagi4205 <nagisome1234@gmail.com>
2023-12-16 10:42:08 +0900

    refactor: create CustomButton

---
+++ src/routes/CustomButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  type labelType = '+' | '-' | '0' | '×';
+
+  export let label: labelType;
+  export let onClick: () => void = () => {};
+  export let className = '';
+
+  const textColor = label === "×" ? "text-black" : "text-white";
+</script>
+
+<button class={`w-8 h-8 ${textColor} ${className}`} tabindex="-1" on:click={onClick}>
+  {label}
+</button> 
